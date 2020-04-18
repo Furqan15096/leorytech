906909e24899d99bdea2bffed69bbecc1ec1cbb0
Furqan15096 <sialbunny@gmail.com>
2020-04-18 15:59:44 +0500

    update

--- src/component/dashboard/contactUS/contactUs.js
+++ src/component/dashboard/contactUS/contactUs.js
@@ -3,43 +3,37 @@ import { Container } from 'reactstrap';
 import "./contactUs.css"
 
 
- class Cont extends React.Component {
+class Cont extends React.Component {
     render() {
         return <>
             <Container className="themed-container">
-                <div>
-
-                
-
-                <div className="container_main_div">
-                    <div className="inner_container_main_div">
-                        <div className="text_div">
-                            <h1 className="head_text">
-                                Contact us today
+                <div className="container-div">
+                    <div className="container_main_div">
+                        <div className="inner_container_main_div">
+                            <div className="text_div">
+                                <h1 className="head_text">
+                                    Contact us today
                             </h1>
-                            <p className="para_text">
-                            Get the right service, determine what you 
-                            need at any time using the contact form for 
-                            inquiries and questions about our services.
+                                <p className="para_text">
+                                    Get the right service, determine what you
+                                    need at any time using the contact form for
+                                    inquiries and questions about our services.
                             </p>
-
-                            <div className="cards_main_div">
-                                <div className="card1_div">
-                                    <div className="cardDiv">
-
-                                    </div>
+                            </div>
+                            <div className="card_main_div">
+                                <div className="card1">
 
                                 </div>
+                                <div className="card1">
 
+                                </div>
+                                <div className="card1">
+
+                                </div>
                             </div>
-
                         </div>
                     </div>
-
                 </div>
-                
-                </div>
-
             </Container>
 
         </>
